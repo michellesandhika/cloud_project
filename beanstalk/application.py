21de863a1696a2f9a56bd613876d883add890e85
michellesandhika <michellesandhika@gmail.com>
2022-04-21 18:17:21 +0800

    modified to add new data

--- beanstalk/application.py
+++ beanstalk/application.py
@@ -1,4 +1,22 @@
 from flask import Flask, render_template, request
+import datetime
+from turtle import speed
+import mysql.connector
+import json
+from time import time, sleep
+    
+def db_connection():
+   mydb = mysql.connector.connect( host = 'cloudcompproject.c5v4sxvaiqcz.us-east-1.rds.amazonaws.com',
+   user = 'clouduser',
+   port = '3306',
+   database = '',
+   passwd = 'cloudcomputing',
+   autocommit = True)
+   
+   return mydb
+
+mydb = db_connection()
+cur = mydb.cursor()
 
 # init flask app
 application = Flask(__name__)
@@ -53,28 +71,81 @@ def index():
 
 @application.route('/driver')
 def driver():
+    
     # get query
     args = request.args.to_dict()
     driver = args['id']
 
     # data format
     drivers = ['name01', 'name02', 'name03', 'name04', 'name05', 'name06', 'name07', 'name08', 'name09', 'name10']
+
+    global date_and_time
+    date_and_time = datetime.datetime(2017, 1, 1, 8, 0, 0)
+    lastTime = datetime.datetime(2017, 1, 1, 23, 59, 59)
+    
+    
+    # data format
+    # drivers = ['likun1000003', 'haowei1000008', 'zouan1000007', 'zengpeng1000000', 'xiexiao1000001', 'shenxian1000004', 'panxian1000005', 'hanhui1000002', 'duxu1000009', 'xiezhi1000006']
+    
+    speedArrayTotal = [['Date', 'Speed'], ["2017-1-1 08:00:00", 0]]
+    
     data = {
         'id': 'name01',
         'plate': '华 AEB132',
         'summary': {},
         'stats': [],
-        'speed': [
-            ['Date', 'Speed'],
-            ['2017-01-01 22:35:50', 66],
-            ['2017-01-01 22:35:50', 68],
-            ['2017-01-01 22:35:50', 68],
-            ['2017-01-01 22:35:50', 68],
-            ['2017-01-01 22:35:50', 68],
-        ],
+        'speed': speedArrayTotal,
+        'lastOverspeed': 0,
     }
-
+    
+    # data = {
+    #     'id': 'name01',
+    #     'plate': '华 AEB132',
+    #     'summary': {},
+    #     'stats': [],
+    #     'speed': [
+    #         ['Date', 'Speed'],
+    #         ['2017-01-01 22:35:50', 66],
+    #         ['2017-01-01 22:35:50', 68],
+    #         ['2017-01-01 22:35:50', 68],
+    #         ['2017-01-01 22:35:50', 68],
+    #         ['2017-01-01 22:35:50', 68],
+    #     ],
+    # }
+    
     return render_template('index.html', drivers=drivers, data=data)
+        
+    # while date_and_time < lastTime:
+    #     sleep(30 - time() % 30)
+    #     time_change = datetime.timedelta(seconds=30)
+    #     new_time = date_and_time + time_change
+    #     date_and_time = new_time
+        
+    #     sql = "SELECT * FROM userTable.partBTable WHERE driverID=\"{0}\" and time < \"{1}\";".format(driver, date_and_time)
+    #     ret = cur.execute(sql)
+    #     result = cur.fetchall()
+        
+    #     speedArrayTotal = []
+    #     speedArrayTotal.append(['Date', 'Speed'])
+
+    #     for i in range(0, len(result)):
+    #         speedArray = []
+    #         speedArray.append(result[i][1])
+    #         speedArray.append(result[i][2].strftime("%Y%m%d , %H:%M:%S"))
+    #         speedArrayTotal.append(speedArray)
+            
+    #     lastOverspeed = result[len(result)-1][3]
+        
+    #     data = {
+    #         'id': 'name01',
+    #         'plate': '华 AEB132',
+    #         'summary': {},
+    #         'stats': [],
+    #         'speed': speedArrayTotal,
+    #         'lastOverspped': lastOverspeed,
+    #     }
+
+    #     return render_template('index.html', drivers=drivers, data=data)
 
 
 if __name__ == '__main__':
