ac2626e1297b76aa78d6df2b9cc3eb63d1b6a21f
battheresa <chawisa.batt@gmail.com>
2022-04-21 16:35:07 +0800

    change ui

--- beanstalk/application.py
+++ beanstalk/application.py
@@ -9,6 +9,7 @@ def index():
     drivers = ['name01', 'name02', 'name03', 'name04', 'name05', 'name06', 'name07', 'name08', 'name09', 'name10']
     data = {
         'id': '',
+        'plate': '',
         'summary': {
             'averageSpeed': '1',
             'countFatigueDriving': '2',
@@ -18,7 +19,34 @@ def index():
             'totalNeutralSlide': '6',
             'countOverSpeed': '7',
             'totalOverSpeed': '8',
-        }
+        },
+        'stats': [
+            {
+                'id': 'name01',
+                'plate': '华 AEB132',
+                'averageSpeed': '1',
+                'countFatigueDriving': '2',
+                'countHthrottleStop': '3',
+                'countOilLeak': '4',
+                'countNeutralSlide': '5',
+                'totalNeutralSlide': '6',
+                'countOverSpeed': '7',
+                'totalOverSpeed': '8',
+            },
+            {
+                'id': 'name02',
+                'plate': '华 AEB132',
+                'averageSpeed': '1',
+                'countFatigueDriving': '2',
+                'countHthrottleStop': '3',
+                'countOilLeak': '4',
+                'countNeutralSlide': '5',
+                'totalNeutralSlide': '6',
+                'countOverSpeed': '7',
+                'totalOverSpeed': '8',
+            }
+        ],
+        'speed': []
     }
     
     return render_template('index.html', drivers=drivers, data=data)
@@ -34,6 +62,8 @@ def driver():
     data = {
         'id': 'name01',
         'plate': '华 AEB132',
+        'summary': {},
+        'stats': [],
         'speed': [
             ['Date', 'Speed'],
             ['2017-01-01 22:35:50', 66],
